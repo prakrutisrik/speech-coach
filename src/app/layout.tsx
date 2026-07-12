69731436e465a99fec0768096b811151c7838f3f
prakruti srikanth <prakrutisrikanth@prakrutis-mbp.lan>
2026-07-12 13:23:16 -0400

    fix vercel build errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,27 +3,32 @@ import { AuthProvider } from "@/components/AuthProvider";
 import { Header } from "@/components/Header";
 import Footer from "@/components/Footer";
 import { createClient } from "@/lib/supabase/server";
-import "./globals.css"
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Speech Coach",
   description: "Practice public speaking with timed prompts and AI feedback."
 };
 
-export default function RootLayout({
-  children
-}: Readonly<{
+export default async function RootLayout({
+  children,
+}: {
   children: React.ReactNode;
-}>) {
+}) {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   return (
     <html lang="en">
       <body>
-        <AuthProvider >
-        <Header />
-      <main>{children}</main>
-        <Footer />
+        <AuthProvider initialUser={user}>
+          <Header />
+          <main>{children}</main>
+          <Footer />
         </AuthProvider>
-        </body>
+      </body>
     </html>
   );
 }
